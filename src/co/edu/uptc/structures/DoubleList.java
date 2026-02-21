33c0c31379b4ed2f2356d4096e5a323d1e57eca4
Yisel-Matabajoy <yisel.matabajoy@uptc.edu.co>
2026-02-21 16:23:09 -0500

    Cambios metodo RemoveAll

--- src/co/edu/uptc/structures/DoubleList.java
+++ src/co/edu/uptc/structures/DoubleList.java
@@ -7,76 +7,127 @@ import java.util.List;
 import java.util.ListIterator;
 
 public class DoubleList<T> implements List<T> {
-    private Node<T> head;
-    private Node<T> tail;
-    private int size;
-    
-    public DoubleList() {
-    	head = null;
-    	tail = null;
-    	size = 0;
-    }
-    
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
+
+	public DoubleList() {
+		head = null;
+		tail = null;
+		size = 0;
+	}
+
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean contains(Object o) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public Iterator<T> iterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public Object[] toArray() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public <T> T[] toArray(T[] a) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public boolean add(T e) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean remove(Object o) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean containsAll(Collection<?> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean addAll(Collection<? extends T> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean addAll(int index, Collection<? extends T> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		if (c == null) {
+			throw new NullPointerException("La collecion esta nula");
+		}
+		Node<T> temporalNode = head;
+		boolean isDeleted = false;
+		while (temporalNode != null) {
+			Node<T> nextNode = temporalNode.getNext();
+			isDeleted = exists(temporalNode, c);
+			temporalNode = nextNode;
+		}
+		return isDeleted;
 	}
+
+	private boolean exists(Node<T> temporalNode, Collection<?> c) {
+		boolean isDeleted = false;
+		if (c.contains(temporalNode.getValue())) {
+			deleteNode(temporalNode);
+			isDeleted = true;
+		}
+		return isDeleted;
+	}
+
+	private void deleteNode(Node<T> temporalNode) {
+		if (temporalNode.getPrevious() == null) {
+			rewriteHead(temporalNode);
+		} else if (temporalNode.getNext() == null) {
+			tail = temporalNode.getPrevious();
+			tail.setNext(null);
+		} else {
+			temporalNode.getPrevious().setNext(temporalNode.getNext());
+			temporalNode.getNext().setPrevious(temporalNode.getPrevious());
+		}
+	}
+
+	private void rewriteHead(Node<T> temporalNode) {
+		head = temporalNode.getNext();
+		if (head != null) {
+			head.setPrevious(null);
+		} else {
+			tail = null;
+		}
+	}
+
 	@Override
 	public boolean retainAll(Collection<?> c) {
 		boolean hasChanged = false;
@@ -92,56 +143,65 @@ public class DoubleList<T> implements List<T> {
 
 		return hasChanged;
 	}
+
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public T get(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public T set(int index, T element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public void add(int index, T element) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	@Override
 	public T remove(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public int indexOf(Object o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
 	@Override
 	public int lastIndexOf(Object o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
 	@Override
 	public ListIterator<T> listIterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public ListIterator<T> listIterator(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public List<T> subList(int fromIndex, int toIndex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-    
 }
